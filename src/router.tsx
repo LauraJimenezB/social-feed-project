911315e49d67a2e4516b177bbb62b234d8d45d5b
LauraJimenezB <laura271260@gmail.com>
2025-11-15 06:28:27 -0500

    add PostPage route

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthPage from "./pages/AuthPage";
 import "@radix-ui/themes/styles.css";
+import PostPage from "./pages/PostPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/signUp", element: <AuthPage /> },
   { path: "/signIn", element: <AuthPage /> },
+  { path: "/post", element: <PostPage /> },
 ]);
